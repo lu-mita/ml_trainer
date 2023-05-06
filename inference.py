3dbb06ce8f89283fc317b1e29dcb059e8b7fbf4d
Gianluca Calò <gianluca.calo11@gmail.com>
2023-05-06 19:58:43 +0200

    added predictions on batch

--- inference.py
+++ inference.py
@@ -27,23 +27,26 @@ class EmotionRecognizer:
         self.model.eval()
 
     def recognize(self, text, treshold=0.5) -> str:
-        predictions = self.get_raw_predictions(text, treshold)
+        predictions = self.get_raw_prediction(text, treshold=treshold)
         predictions = self.le.inverse_transform(predictions)
-        prediction = list(predictions[0])
-        texual_predictions = ", ".join(prediction)
-        return texual_predictions
+        return ", ".join(predictions[0])
 
-    def get_raw_predictions(self, text, treshold=0.5) -> np.ndarray:
+    def recognize_batch(self, batch, t=0.5) -> list:
+        predictions = self.get_raw_predictions(batch, treshold=t)
+        predictions = self.le.inverse_transform(predictions)
+        return [", ".join(pred) for pred in predictions]
+
+    def get_raw_prediction(self, text, treshold=0.5) -> np.ndarray:
         """
-        Returns the normalized preditions of the model in binary format
-        Normalized preditions are obtained by applying a sigmoid function to the raw preditions and then applying a treshold
+        Returns the normalized preditions of the model in binary format.
+        Normalized preditions are obtained by applying a sigmoid function
+        to the raw preditions and then applying a treshold.
         """
         encodings = self.tokenizer(text, add_special_tokens=True)
-
         encodings = {
             k: torch.tensor(v, dtype=self.type_map[k]) \
-                     .unsqueeze(0) \
-                     .to(self.device) for k, v in encodings.items()
+              .unsqueeze(0) \
+              .to(self.device) for k, v in encodings.items()
         }
 
         with torch.no_grad():
@@ -51,3 +54,20 @@ class EmotionRecognizer:
             predictions = torch.sigmoid(output.logits)
             predictions = torch.where(predictions > treshold, 1, 0)
             return predictions.detach().numpy()
+    
+    def get_raw_predictions(self, batch, treshold=0.5)-> np.ndarray:
+      encodings = self.tokenizer(
+          batch,
+          padding="max_length",
+          max_length=512,
+          add_special_tokens=True
+      )
+      encodings = {
+          k:torch.tensor(v, dtype=self.type_map[k]) \
+            .to(self.device) for k, v in encodings.items()
+      }
+      with torch.no_grad():
+        output = self.model(**encodings)
+        predictions = torch.sigmoid(output.logits)
+        predictions = torch.where(predictions > treshold, 1, 0)
+        return predictions.detach().numpy()
